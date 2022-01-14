aed080605bc919f5c05a811c5892812f9d163b17
MIRIAM NAKI <nakiyingim@gmail.com>
2022-01-14 14:55:47 -0800

    added review section

--- client/src/components/TestimonyGroup/TestimonyGroup.js
+++ client/src/components/TestimonyGroup/TestimonyGroup.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './TestimonyGroup.scss';
-import { Testimonials } from '../Testimonials/Testimonials';
+import  Testimonials  from '../Testimonials/Testimonials';
 
 const TestimonyGroup = () => {
   return (
     <div className='testimonies'>
-      <h1 className='testimonies__title'>Testimonials</h1>
+      <h1 className='testimonies__title'>Reviews</h1>
+      <p className='testimonies__rating-wraper'><span className='testimonies__rating'>4.5</span><span className='testimonies__review'>(4 reviews)</span></p>
       <div className='testimonies__wrapper'>
         <Testimonials/>
-        <Testimonials/>
-        <Testimonials/>
-        <Testimonials/>
+        
 
       </div>
       
