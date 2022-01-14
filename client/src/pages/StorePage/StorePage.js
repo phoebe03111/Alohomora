b9d4cbb533cd03efa786c21e52eb6dc1ea2d3ab0
MIRIAM NAKI <nakiyingim@gmail.com>
2022-01-13 22:04:40 -0800

    add testimonial component

--- client/src/pages/StorePage/StorePage.js
+++ client/src/pages/StorePage/StorePage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Testimonials } from "../../components/Testimonials/Testimonials";
 
 const StorePage = () => {
-  return <div>This is from Store Page</div>;
+  return (
+  <div>
+    This is from Store Page
+    <Testimonials/>
+    </div>
+    );
 };
 
 export default StorePage;
