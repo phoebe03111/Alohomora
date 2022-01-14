b28d909bbb1d5956079f006e95c48aeb7897114b
Phoebe Chang <phoebe03111@gmail.com>
2022-01-13 18:42:52 -0800

    Create ExplorePage and CardGroup

---
+++ client/src/components/CardGroup/CardGroup.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './CardGroup.scss';
+
+function CardGroup() {
+  return <div>CardGroup</div>;
+}
+
+export default CardGroup;
---
+++ client/src/pages/ExplorePage/ExplorePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './ExplorePage.scss';
+
+function ExplorePage() {
+  return <div>explore page</div>;
+}
+
+export default ExplorePage;
