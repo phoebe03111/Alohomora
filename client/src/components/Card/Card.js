1e36a4cbc60307d1bcca0734b5c9fcb9e98c16a4
hnslyswhy <huanyuw01@gmail.com>
2022-01-13 16:26:34 -0800

    create card with basic responsive styles

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,9 +1,19 @@
 import React from "react";
-import styles from "./Card.module.css";
+import "./Card.scss";
 
-function Card(props) {
+function Card() {
+  let fakeData = {
+    name: "Four Barrel Coffee",
+    image_url:
+      "http://s3-media2.fl.yelpcdn.com/bphoto/MmgtASP3l_t4tPCL1iAsCg/o.jpg",
+  };
   return (
-    <div className={`${styles.card} ${props.className}`}>{props.children}</div>
+    <div className="card">
+      <h1 className="card__title">{fakeData.name}</h1>
+      <div className="card__img-container">
+        <img className="card__img" src={fakeData.image_url} alt="" />
+      </div>
+    </div>
   );
 }
 
