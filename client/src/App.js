ab4a66504c8f54ec7fa054a8802eba4229fdc3d8
hnslyswhy <huanyuw01@gmail.com>
2022-01-13 20:51:52 -0800

    created filter modal

--- client/src/App.js
+++ client/src/App.js
@@ -10,15 +10,17 @@ import HomePage from "./pages/HomePage/HomePage";
 import CategoryPage from "./pages/CategoryPage/CategoryPage";
 import StorePage from "./pages/StorePage/StorePage";
 import Header from "./components/Header/Header";
+import Modal from "./components/Modal/Modal";
 
 // the routes here are open to changes
 function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Switch>
         <Route path="/category/:id" component={CategoryPage} />
         <Route path="/store/:id" component={StorePage} />
+        <Route path="/test" component={Modal} />
         <Route path="/" exact component={HomePage} />
         <Route path="*">
           <Redirect to="/" exact component={HomePage} />
